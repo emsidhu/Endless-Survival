759b8f180386c6baec4fb74d27f19d13eb309fa0
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2022-04-29 12:43:11 -0700

    Fixed Gameover Screen

--- Menus/GameOver.gd
+++ Menus/GameOver.gd
@@ -2,12 +2,9 @@ extends Control
 
 
 func _ready() -> void:
-	
 	$Title.text = "Your Score Was " + str(Globals.score)
-	PlayerStats.resetStats()
-
-
+	$Menu/ChangeSceneBtn.grab_focus()
 
 
 func _on_ChangeSceneBtn_button_up():
-	PlayerStats.health = PlayerStats.max_health
+	PlayerStats.resetStats()
